ffc128c980178e547e87623e84093e4c8e5f2d3e
yetti7 <taylormedley7@gmail.com>
2024-10-02 23:46:56 -0400

    update hover shadow on icn container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,33 +21,33 @@ export default function Home() {
 
       <div className="grid grid-cols-3 gap-8">
         {/* Top Row */}
-        <a href="https://abs.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://abs.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <AudiobookshelfIcon className="w-16 h-16 text-white" />
         </a>
-        <a href="https://app.plex.tv" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://app.plex.tv" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <PlexIcon className="w-16 h-16 text-white" />
         </a>
-        <a href="https://discord.gg/dXwYnad" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://discord.gg/dXwYnad" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <DiscordIcon className="w-16 h-16 text-white" />
         </a>
         {/* Middle Row */}
-        <a href="https://sonarr.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://sonarr.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <Image src={SonarrIcon} alt="Sonarr" width={64} height={64} />
         </a>
-        <a href="https://radarr.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://radarr.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <Image src={RadarrIcon} alt="Radarr" width={64} height={64} />
         </a>
-        <a href="https://readarr.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://readarr.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <Image src={ReadarrIcon} alt="Readarr" width={64} height={64} />
         </a>
         {/* Bottom Row */}
-        <a href="https://sab.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://sab.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <SabnzbIcon className="w-16 h-16 text-white" />
         </a>
-        <a href="https://tautulli.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://tautulli.deepnorth.app" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <Image src={TautulliIcon} alt="Tautulli" width={64} height={64} />
         </a>
-        <a href="https://syncthing.net" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-gray-500/50 hover:scale-105 transition-all duration-300">
+        <a href="https://syncthing.net" target="_blank" rel="noopener noreferrer" className="bg-gray-900 p-4 rounded-lg border border-gray-700 shadow-lg hover:shadow-[0_4px_10px_rgba(255,204,0,0.6)] hover:scale-105 transition-all duration-300">
           <SyncthingIcon className="w-16 h-16 text-white" />
         </a>
       </div>
