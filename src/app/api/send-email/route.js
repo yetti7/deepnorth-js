d4fd624d972231dce7940376447a4a670d79aaa2
yetti7 <taylormedley7@gmail.com>
2025-02-10 21:09:23 -0500

    Update route.js

--- src/app/api/send-email/route.js
+++ src/app/api/send-email/route.js
@@ -2,7 +2,7 @@ import sgMail from "@sendgrid/mail";
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 
 export async function POST(request) {
   try {
